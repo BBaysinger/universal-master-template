1e636e184371bd03502a7352231308e9adaf6526
Bradley Baysinger <bradleybaysinger@gmail.com>
2022-04-08 02:15:47 -0700

    Optimizing output for html email.

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+/**
+ * @file Outputs HTML master templates for SFMC, optimized for email.
+ * @author Bradley Baysinger
+ */
+const gulp = require("gulp");
+const beautify = require("gulp-beautify");
+const strip = require("gulp-strip-comments");
+const htmlmin = require("gulp-htmlmin");
+
+function defaultTask(cb) {
+  // TODO: Determine what should be the default task, if anything.
+  cb();
+}
+
+/**
+ * Output minified version... Possibly for produciton if we determing that's a good idea.
+ */
+gulp.task("prod", function () {
+  const mOpts = {};
+  mOpts.collapseWhitespace = true;
+  mOpts.minifyCSS = true;
+  mOpts.processConditionalComments = true;
+  return (
+    gulp
+      // Find the HTML.
+      .src("./*.html")
+      // Remove all comments without removing conditionals for Outlook and whatnot.
+      .pipe(strip({ safe: true }))
+      // Remove all newlines and redunant whitespace.
+      .pipe(htmlmin(mOpts))
+      // Output to `dist` directory.
+      .pipe(gulp.dest("./dist/"))
+  );
+});
+
+/**
+ * Output readable version for the sandbox.
+ */
+gulp.task("sandbox", function () {
+  // Beautify options.
+  let bOpts = {};
+  bOpts.newline_between_rules = false;
+  bOpts.selector_separator_newline = false;
+  bOpts.indent_size = 2;
+  // Minify options.
+  const mOpts = {};
+  mOpts.collapseWhitespace = true;
+  mOpts.minifyCSS = true;
+  mOpts.processConditionalComments = true;
+  return (
+    gulp
+      // Find the HTML.
+      .src("./*.html")
+      // Remove all comments except Outlook conditionals and other special comments.
+      .pipe(strip({ safe: true }))
+      // Minify to only remove all newlines and redunant whitespace, which beautify wont do.
+      .pipe(htmlmin(mOpts))
+      // Make it pretty again.
+      .pipe(beautify.html(bOpts))
+      // Output to `dist` directory.
+      .pipe(gulp.dest("./dist/"))
+  );
+});
+
+exports.default = defaultTask;
